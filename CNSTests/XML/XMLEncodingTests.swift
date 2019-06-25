ef6ee57ef7c244e4c5fb522e2ce701b45fc208e1
ライアン <ryan.wachowski@duethealth.com>
2019-06-25 12:53:39 -0400

    Fixed up the deeply-nested test and discovered a bug.

--- CNSTests/XML/XMLEncodingTests.swift
+++ CNSTests/XML/XMLEncodingTests.swift
@@ -26,7 +26,7 @@ struct GetPriceRequest: Encodable, Equatable {
     }
 
     struct Price: Encodable, Equatable {
-        let quantity: Double
+        let amount: Double
         let units: String
         let discount: Double
     }
@@ -59,12 +59,14 @@ class XMLEncodingTests: XCTestCase {
         let encoder = SOAPEncoder()
         encoder.keyEncodingStrategy = .convertToPascalCase
         encoder.soapNamespaceUsage = .define(using: XMLNamespace(prefix: "soap", uri: "http://www.w3.org/2003/05/soap-envelope/"), including: [])
-        let request = GetPriceRequest(name: "iPhone Excess to the Max", sku: GetPriceRequest.SKU(value: "00124230128212398-6"), availability: GetPriceRequest.Availability(isAvailable: true, stock: 100), price: GetPriceRequest.Price(quantity: 1.9, units: "Dollars", discount: 0.15))
+        encoder.requestPayloadNamespaceUsage = .defineBeneath(XMLNamespace(prefix: "m", uri: "https://www.w3schools.com/prices"))
+        let request = GetPriceRequest(name: "iPhone Excess to the Max", sku: GetPriceRequest.SKU(value: "00124230128212398-6"), availability: GetPriceRequest.Availability(isAvailable: true, stock: 100), price: GetPriceRequest.Price(amount: 1.9, units: "Dollars", discount: 0.15))
         do {
             let data = try encoder.encode(request)
             let expected = """
-            <?xml version="1.0"?><soap:Envelope xmlns:soap="http://www.w3.org/2003/05/soap-envelope/"><soap:Body><m:GetPriceResponse xmlns:m="https://www.w3schools.com/prices"><m:Sku><m:Value>00124230128212398-6</m:Value></m:Sku><m:Availability><m:Price><m:Amount>1.90</m:Amount><m:Units>Dollars</m:Units></m:Price><m:Discount>0.15</m:Discount></m:GetPriceResponse></soap:Body></soap:Envelope>
+            <?xml version="1.0"?><soap:Envelope xmlns:soap="http://www.w3.org/2003/05/soap-envelope/"><soap:Body><m:GetPriceResponse xmlns:m="https://www.w3schools.com/prices"><m:Sku><m:Value>00124230128212398-6</m:Value></m:Sku><m:Availability><m:IsAvailable>true</m:IsAvailable><m:Stock>100</m:Stock></m:Availability><m:Price><m:Amount>1.9</m:Amount><m:Units>Dollars</m:Units><m:Discount>0.15</m:Discount></m:Price></m:GetPriceResponse></soap:Body></soap:Envelope>
             """.data(using: .utf8)!
+            print(String(data: data, encoding: .utf8)!)
             XCTAssertTrue(data == expected)
         } catch { XCTFail(error.localizedDescription) }
     }
