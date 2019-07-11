4305c2eb5f4a0a31b8126b069f9ed87a919c50c4
ライアン <ryan.wachowski@duethealth.com>
2019-07-11 13:51:00 -0400

    Refactors tests to use new syntax

--- KumoTests/Fixtures/Services/PostTests.swift
+++ KumoTests/Fixtures/Services/PostTests.swift
@@ -5,13 +5,13 @@ import XCTest
 class PostTests: NetworkTest {
     
     func testSuccessfulPostRequestWithDynamicBodyEmittingVoid() {
-        successfulTest(of: service.post("post", body: ["one": ["two": 3]]))
+        successfulTest(of: service.perform(HTTP.Request.post("post").body(["one": ["two": 3]])))
             <| "Will eventually emit Void"
             <| always()
     }
     
     func testSuccessfulPostRequestWithDynamicBodyEmittingElement() {
-        successfulTest(of: service.post("anything", body: RequestBody.dynamicBody))
+        successfulTest(of: service.perform(HTTP.Request.post("anything").body(RequestBody.dynamicBody)))
             <| "Will eventually emit the body which was POSTed"
             <| { (response: MockObjectResponse<RequestBody>) in
                 let leaf = RequestBody.dynamicBody["leaf"] as? String
