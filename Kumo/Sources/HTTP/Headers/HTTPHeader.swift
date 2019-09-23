00f99bd9341cd09bd2b452fc775641e7378a27f6
James Power <jim.power@duethealth.com>
2019-09-23 12:24:58 -0400

    Added User-Agent as , using the encoder for content type in HTTPRequests for typed and dynamic bodies (not multipart), replacing AnyEncodable as the root node name for anything AnyEncodable is encoding

--- Kumo/Sources/HTTP/Headers/HTTPHeader.swift
+++ Kumo/Sources/HTTP/Headers/HTTPHeader.swift
@@ -12,6 +12,7 @@ public extension HTTP {
         public static let authorization = HTTP.Header(rawValue: "Authorization")
         public static let contentType = HTTP.Header(rawValue: "Content-Type")
         public static let contentLength = HTTP.Header(rawValue: "Content-Length")
+        public static let userAgent = HTTP.Header(rawValue: "User-Agent")
 
         public static func custom(_ value: String) -> HTTP.Header {
             return HTTP.Header(rawValue: value)
