c241d688db959031a6253fa9eb07c2a2230cf11e
Justin Anderson <jus0610tin@gmail.com>
2020-04-13 10:40:34 -0600

    Readded Header

---
+++ Sources/KumoCoding/KumoCoding.h
@@ -0,0 +1,13 @@
+//
+//  KumoCoding.h
+//  KumoCoding
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for KumoCoding.
+FOUNDATION_EXPORT double KumoCodingVersionNumber;
+
+//! Project version string for KumoCoding.
+FOUNDATION_EXPORT const unsigned char KumoCodingVersionString[];
+
