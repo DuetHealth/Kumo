b60ef32411de5cf51c031c24cb7495eb186cd321
James Power <jep@Jamess-MacBook-Pro.local>
2024-08-24 21:45:51 -0400

    Use proper key for associated objects

--- Sources/Kumo/Extensions/AnyCancellable.swift
+++ Sources/Kumo/Extensions/AnyCancellable.swift
@@ -1,14 +1,14 @@
 import Combine
 import Foundation
 
-fileprivate var cancellablesKey = UInt8.zero
+private var cancellablesKey: Void?
 
 extension Cancellable {
 
     func withLifetime(of object: AnyObject) {
         var cancellables = objc_getAssociatedObject(object, &cancellablesKey) as? [AnyCancellable] ?? [AnyCancellable]()
         AnyCancellable(self).store(in: &cancellables)
-        objc_setAssociatedObject(object, &cancellables, cancellables, .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
+        objc_setAssociatedObject(object, &cancellablesKey, cancellables, .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
     }
 
 }
