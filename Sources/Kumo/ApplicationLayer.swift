ebb1dfd5c60739a72d65ae4a1fc5a37d2a3da87c
James Power <jep@lepoer.us>
2026-03-22 09:08:29 -0400

    Migrate ApplicationLayer from SCNetworkReachability to NWPathMonitor.

--- Sources/Kumo/ApplicationLayer.swift
+++ Sources/Kumo/ApplicationLayer.swift
@@ -1,6 +1,6 @@
-import Combine
+@preconcurrency import Combine
 import Foundation
-import SystemConfiguration
+import Network
 
 /// The network connectivity status.
 public enum NetworkConnectivity {
@@ -24,11 +24,12 @@ public enum NetworkConnectivity {
 /// services and exposes a publisher ``networkConnectivity`` to monitor network
 /// connectivity.
 open class ApplicationLayer {
-    
+
     private var commonHeaders = [String: String]()
     private let services: [ServiceKey: Service]
 
     private let networkConnectivitySubject: CurrentValueSubject<NetworkConnectivity, Never> = .init(.unknown)
+    private let pathMonitor = NWPathMonitor()
 
     /// A publisher that updates with the current network connectivity status
     /// for the device.
@@ -40,18 +41,29 @@ open class ApplicationLayer {
     /// pairs.
     public init(with services: [ServiceKey: Service] = [:]) {
         self.services = services
-        
-        var address = sockaddr_in()
-        address.sin_len = UInt8(MemoryLayout<sockaddr_in>.size)
-        address.sin_family = sa_family_t(AF_INET)
-        withUnsafePointer(to: &address) { pointer in
-            pointer.withMemoryRebound(to: sockaddr.self, capacity: MemoryLayout<sockaddr>.size) {
-                SCNetworkReachabilityCreateWithAddress(nil, $0)
+
+        let subject = networkConnectivitySubject
+        pathMonitor.pathUpdateHandler = { path in
+            let connectivity: NetworkConnectivity
+            switch path.status {
+            case .satisfied:
+                #if os(iOS)
+                connectivity = path.isExpensive ? .wwan : .internet
+                #else
+                connectivity = .internet
+                #endif
+            case .unsatisfied, .requiresConnection:
+                connectivity = .notConnected
+            @unknown default:
+                connectivity = .unknown
             }
+            subject.send(connectivity)
         }
-        .map { [unowned self] in self.publishReachability($0) }?
-        .sink(receiveValue: { [unowned self] in self.networkConnectivitySubject.send($0) })
-        .withLifetime(of: self)
+        pathMonitor.start(queue: DispatchQueue(label: "DuetHealth.Kumo.networkMonitor"))
+    }
+
+    deinit {
+        pathMonitor.cancel()
     }
 
     /// Retrieves the service for a given `key`.
@@ -59,50 +71,6 @@ open class ApplicationLayer {
     /// layer.
     public subscript(_ key: ServiceKey) -> Service {
         return services[key]!
-    }
-
-    private func publishReachability(_ reachability: SCNetworkReachability) -> AnyPublisher<NetworkConnectivity, Never> {
-        AnyPublisher.create { subscriber in
-            var context = SCNetworkReachabilityContext(version: 0, info: nil, retain: nil, release: nil, copyDescription: nil)
-            context.info = Unmanaged.passRetained(AnyObserverReference<NetworkConnectivity, Never>(subscriber)).toOpaque()
-            SCNetworkReachabilitySetCallback(reachability, { _, flags, info in
-                guard let observer = info.map({ Unmanaged<AnyObserverReference<NetworkConnectivity, Never>>.fromOpaque($0).takeUnretainedValue() }) else { return }
-                if flags.isReachable {
-                    #if os(iOS)
-                    observer.base.onNext(flags.contains(.isWWAN) ? .wwan : .internet)
-                    #else
-                    observer.base.onNext(.internet)
-                    #endif
-                } else {
-                    observer.base.onNext(.notConnected)
-                }
-            }, &context)
-            SCNetworkReachabilitySetDispatchQueue(reachability, DispatchQueue.main)
-            return AnyCancellable() {
-                SCNetworkReachabilitySetCallback(reachability, nil, nil)
-                SCNetworkReachabilitySetDispatchQueue(reachability, nil)
-            }
-        }
-    }
-    
-}
-
-private class AnyObserverReference<Input, Failure> where Failure: Error {
-
-    let base: AnyObserver<Input, Failure>
-
-    init(_ base: AnyObserver<Input, Failure>) {
-        self.base = base
-    }
-
-}
-
-private extension SCNetworkReachabilityFlags {
-
-    var isReachable: Bool {
-        let canConnectAutomatically = contains(.connectionOnDemand) || contains(.connectionOnTraffic) && !contains(.interventionRequired)
-        return contains(.reachable)
-            && (!contains(.connectionRequired) || canConnectAutomatically)
     }
 
 }
